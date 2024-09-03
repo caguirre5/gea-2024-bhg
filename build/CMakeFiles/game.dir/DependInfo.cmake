6872e7e1cb4a8edcecd52ec9f9f140a380c7211c
caguirre5 <caguirre.dt@gmail.com>
2024-09-02 19:08:30 -0600

    ejecucion exitosa

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cristian/Documents/Github/gea-2024-bhg/src/Engine/Game.cpp" "CMakeFiles/game.dir/src/Engine/Game.cpp.o" "gcc" "CMakeFiles/game.dir/src/Engine/Game.cpp.o.d"
+  "/home/cristian/Documents/Github/gea-2024-bhg/src/Engine/Graphics/Texture.cpp" "CMakeFiles/game.dir/src/Engine/Graphics/Texture.cpp.o" "gcc" "CMakeFiles/game.dir/src/Engine/Graphics/Texture.cpp.o.d"
+  "/home/cristian/Documents/Github/gea-2024-bhg/src/Engine/Graphics/TextureManager.cpp" "CMakeFiles/game.dir/src/Engine/Graphics/TextureManager.cpp.o" "gcc" "CMakeFiles/game.dir/src/Engine/Graphics/TextureManager.cpp.o.d"
+  "/home/cristian/Documents/Github/gea-2024-bhg/src/Engine/Scene.cpp" "CMakeFiles/game.dir/src/Engine/Scene.cpp.o" "gcc" "CMakeFiles/game.dir/src/Engine/Scene.cpp.o.d"
+  "/home/cristian/Documents/Github/gea-2024-bhg/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/Engine/Game.cpp.o"
+  "CMakeFiles/game.dir/src/Engine/Game.cpp.o.d"
+  "CMakeFiles/game.dir/src/Engine/Graphics/Texture.cpp.o"
+  "CMakeFiles/game.dir/src/Engine/Graphics/Texture.cpp.o.d"
+  "CMakeFiles/game.dir/src/Engine/Graphics/TextureManager.cpp.o"
+  "CMakeFiles/game.dir/src/Engine/Graphics/TextureManager.cpp.o.d"
+  "CMakeFiles/game.dir/src/Engine/Scene.cpp.o"
+  "CMakeFiles/game.dir/src/Engine/Scene.cpp.o.d"
+  "CMakeFiles/game.dir/src/main.cpp.o"
+  "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
